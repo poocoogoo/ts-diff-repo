995ad3f6a0074cadd3fbbbf7db6e04be49eeb0c5
刘建晶 <liujianjing@meituan.com>
2022-02-10 12:48:44 +0800

    修改了被引用的工具类

--- ReactNative/RNUtils.ts
+++ ReactNative/RNUtils.ts
@@ -1,3 +1,3 @@
 import {Env} from "./Env";
 
-export const HomeURL = Env.isIOS() ? 'http://xxxxxxxyyyyyzzzz.ios.com' : 'http://xxxxxxxyyyyyzzzz.android.com';
+export const HomeURL = Env.isIOS() ? 'http://xxxxxxxyyyyyzzzz.ios.cn' : 'http://xxxxxxxyyyyyzzzz.android.cn';
