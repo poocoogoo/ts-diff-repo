918ea03b02abc85e4b0580bbd2340f8f923b3cc5
刘建晶 <liujianjing@meituan.com>
2022-02-10 16:07:30 +0800

    业务端采用变量引用

--- ReactNative/Header.tsx
+++ ReactNative/Header.tsx
@@ -3,17 +3,13 @@ import {HomeURL, iOSLogicFunc} from "./RNUtils";
 import { WMEnv} from "./WMEnv";
 
 const isIOS = Env.isIOS();
+const isWM = Env.isWM();
 
 class Header {
 	
 	render() {
 		console.log("reuse");
-		let scheme = '';
-		if (Env.isWM()) {
-			scheme = 'ddd_wm';
-		} else {
-			scheme = 'ddd_channel';
-		}
+		const scheme = isWM ? 'xxx_wm' : 'ddd_channel';
 		if (scheme.length > 0) {
 			console.log("跳转前的逻辑");
 			console.log("跳转scheme");
