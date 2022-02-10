e7e2fe83c825a77a428df694aa6136223d43eb3f
刘建晶 <liujianjing@meituan.com>
2022-02-10 16:14:41 +0800

    删掉业务端

--- ReactNative/Header.tsx
+++ ReactNative/Header.tsx
@@ -9,14 +9,5 @@ class Header {
 	
 	render() {
 		console.log("reuse");
-		const scheme = isWM ? 'xxx_wm' : 'ddd_channel';
-		if (scheme.length > 0) {
-			console.log("跳转前的逻辑");
-			console.log("跳转scheme");
-		} else {
-			console.log("不跳转scheme");
-		}
-		const targetScheme = scheme + 'postURL';
-		console.log(targetScheme);
 	}
 }
