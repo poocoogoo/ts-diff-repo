2b482f44e93b69fc518500c8fdedeb0e5075418a
刘建晶 <liujianjing@meituan.com>
2022-02-11 16:17:49 +0800

    新增Mach的View

---
+++ Mach/Index.tsx
@@ -0,0 +1,10 @@
+import {Component} from "react";
+import {View, ScrollViewComponent} from "react-native";
+
+class MachView extends Component<any, any> {
+	render() {
+		return (<View>
+		
+		</View>);
+	}
+}
