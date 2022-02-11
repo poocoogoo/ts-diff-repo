0a64181674a86ba23f59cbe4fd6f1ceeee40936a
刘建晶 <liujianjing@meituan.com>
2022-02-11 16:31:55 +0800

    Mach多端差异

--- Mach/Index.tsx
+++ Mach/Index.tsx
@@ -1,10 +1,13 @@
 import {Component} from "react";
-import {View, ScrollViewComponent} from "react-native";
+import {View, ScrollViewComponent, Text} from "react-native";
 
 class MachView extends Component<any, any> {
 	render() {
+		const isIOS = this.props.env.osName === 'ios';
+		const isAndroid = this.props.env.osName === 'android';
 		return (<View>
-		
+			{ isAndroid && <Text> Android </Text>}
+			{ isIOS && <Text> iOS logic</Text>}
 		</View>);
 	}
 }
