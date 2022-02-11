2a4c70bef4e2ec15e7fa3ede32fcae4fa4f053ce
刘建晶 <liujianjing@meituan.com>
2022-02-11 15:50:12 +0800

    新增NativeModule

--- ReactNative/Header.tsx
+++ ReactNative/Header.tsx
@@ -1,13 +1,16 @@
 import { Env, WMEnv, iOSLogicFunc, appBizId} from "ts-diff-common";
+import {NativeModules} from "react-native";
 
 const isIOS = Env.isIOS();
 const isWM = Env.isWM();
 
+const shareModule = NativeModules.RNShareModule;
 class Header {
 	
 	render() {
 		const bizId = appBizId();
 		console.log(bizId);
 		console.log("reuse");
+		shareModule();
 	}
 }
