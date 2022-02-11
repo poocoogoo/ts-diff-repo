848fa182530c1b080e2a7972bfd049fd955d0fb7
刘建晶 <liujianjing@meituan.com>
2022-02-11 16:48:15 +0800

    引入了NativeModule

--- Mach/Index.tsx
+++ Mach/Index.tsx
@@ -1,10 +1,16 @@
 import {Component} from "react";
 import {View, ScrollViewComponent, Text, Image} from "react-native";
+import {MachNative} from "./MachNative";
 
 class MachView extends Component<any, any> {
 	render() {
 		const isIOS = this.props.env.osName === 'ios';
 		const isAndroid = this.props.env.osName === 'android';
+		const param = {
+			currentName: 'Index',
+			cardName: 'Home'
+		}
+		MachNative.sendEvent('jumpToDetail', JSON.stringify(param))
 		return (<View>
 			{ isAndroid && <Text> Android </Text>}
 			{ isIOS && <Text> iOS logic</Text>}
---
+++ Mach/MachNative.ts
@@ -0,0 +1,8 @@
+
+function sendEventFunc(eventName: string, paramJsonStr: string) {
+	/// Native logic
+}
+
+export const MachNative = {
+	sendEvent: sendEventFunc
+}
